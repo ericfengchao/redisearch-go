5f3061045b579bdf920b3286ebe35a1651c09d1a
ericfengchao <eric.feng@thecarousell.com>
2019-08-03 18:40:28 +0800

    migration setup

--- redisearch/redisearch_test.go
+++ redisearch/redisearch_test.go
@@ -1,4 +1,4 @@
-package redisearch_test
+package redisearch
 
 import (
 	"fmt"
@@ -7,53 +7,52 @@ import (
 	"testing"
 	"time"
 
-	"github.com/RediSearch/redisearch-go/redisearch"
 	"github.com/stretchr/testify/assert"
 )
 
-func createClient(indexName string) *redisearch.Client {
+func createClient(indexName string) *Client {
 	value, exists := os.LookupEnv("REDISEARCH_TEST_HOST")
 	host := "localhost:6379"
 	if exists && value != "" {
 		host = value
 	}
-	return redisearch.NewClient(host, indexName)
+	return NewClient(host, indexName)
 }
 
-func createAutocompleter(indexName string) *redisearch.Autocompleter {
+func createAutocompleter(indexName string) *Autocompleter {
 	value, exists := os.LookupEnv("REDISEARCH_TEST_HOST")
 	host := "localhost:6379"
 	if exists && value != "" {
 		host = value
 	}
-	return redisearch.NewAutocompleter(host, indexName)
+	return NewAutocompleter(host, indexName)
 }
 
 func TestClient(t *testing.T) {
 
-	c := createClient("testung")
+	c := createClient("testing")
 
-	sc := redisearch.NewSchema(redisearch.DefaultOptions).
-		AddField(redisearch.NewTextField("foo"))
+	sc := NewSchema(DefaultOptions).
+		AddField(NewTextField("foo"))
 	c.Drop()
 	if err := c.CreateIndex(sc); err != nil {
 		t.Fatal(err)
 	}
 
-	docs := make([]redisearch.Document, 100)
+	docs := make([]Document, 100)
 	for i := 0; i < 100; i++ {
-		docs[i] = redisearch.NewDocument(fmt.Sprintf("doc%d", i), float32(i)/float32(100)).Set("foo", "hello world")
+		docs[i] = NewDocument(fmt.Sprintf("doc%d", i), float32(i)/float32(100)).Set("foo", "hello world")
 	}
 
-	if err := c.IndexOptions(redisearch.DefaultIndexingOptions, docs...); err != nil {
+	if err := c.IndexOptions(DefaultIndexingOptions, docs...); err != nil {
 		t.Fatal(err)
 	}
 
 	// Test it again
-	if err := c.IndexOptions(redisearch.DefaultIndexingOptions, docs...); err == nil {
+	if err := c.IndexOptions(DefaultIndexingOptions, docs...); err == nil {
 		t.Fatal("Expected error for duplicate document")
 	} else {
-		if merr, ok := err.(redisearch.MultiError); !ok {
+		if merr, ok := err.(MultiError); !ok {
 			t.Fatal("error not a multi error")
 		} else {
 			assert.Equal(t, 100, len(merr))
@@ -62,7 +61,7 @@ func TestClient(t *testing.T) {
 		}
 	}
 
-	docs, total, err := c.Search(redisearch.NewQuery("hello world"))
+	docs, total, err := c.Search(NewQuery("hello world"))
 
 	assert.Nil(t, err)
 	assert.Equal(t, 100, total)
@@ -74,9 +73,9 @@ func TestClient(t *testing.T) {
 func TestInfo(t *testing.T) {
 	c := createClient("testung")
 
-	sc := redisearch.NewSchema(redisearch.DefaultOptions).
-		AddField(redisearch.NewTextField("foo")).
-		AddField(redisearch.NewSortableNumericField("bar"))
+	sc := NewSchema(DefaultOptions).
+		AddField(NewTextField("foo")).
+		AddField(NewSortableNumericField("bar"))
 	c.Drop()
 	assert.Nil(t, c.CreateIndex(sc))
 
@@ -88,25 +87,25 @@ func TestInfo(t *testing.T) {
 func TestNumeric(t *testing.T) {
 	c := createClient("testung")
 
-	sc := redisearch.NewSchema(redisearch.DefaultOptions).
-		AddField(redisearch.NewTextField("foo")).
-		AddField(redisearch.NewSortableNumericField("bar"))
+	sc := NewSchema(DefaultOptions).
+		AddField(NewTextField("foo")).
+		AddField(NewSortableNumericField("bar"))
 	c.Drop()
 	assert.Nil(t, c.CreateIndex(sc))
 
-	docs := make([]redisearch.Document, 100)
+	docs := make([]Document, 100)
 	for i := 0; i < 100; i++ {
-		docs[i] = redisearch.NewDocument(fmt.Sprintf("doc%d", i), 1).Set("foo", "hello world").Set("bar", i)
+		docs[i] = NewDocument(fmt.Sprintf("doc%d", i), 1).Set("foo", "hello world").Set("bar", i)
 	}
 
 	assert.Nil(t, c.Index(docs...))
 
-	docs, total, err := c.Search(redisearch.NewQuery("hello world @bar:[50 100]").SetFlags(redisearch.QueryNoContent | redisearch.QueryWithScores))
+	docs, total, err := c.Search(NewQuery("hello world @bar:[50 100]").SetFlags(QueryNoContent | QueryWithScores))
 	assert.Nil(t, err)
 	assert.Equal(t, 10, len(docs))
 	assert.Equal(t, 50, total)
 
-	docs, total, err = c.Search(redisearch.NewQuery("hello world @bar:[40 90]").SetSortBy("bar", false))
+	docs, total, err = c.Search(NewQuery("hello world @bar:[40 90]").SetSortBy("bar", false))
 	assert.Nil(t, err)
 	assert.Equal(t, 10, len(docs))
 	assert.Equal(t, 51, total)
@@ -114,7 +113,7 @@ func TestNumeric(t *testing.T) {
 	assert.Equal(t, "doc89", docs[1].Id)
 	assert.Equal(t, "doc81", docs[9].Id)
 
-	docs, total, err = c.Search(redisearch.NewQuery("hello world @bar:[40 90]").
+	docs, total, err = c.Search(NewQuery("hello world @bar:[40 90]").
 		SetSortBy("bar", true).
 		SetReturnFields("foo"))
 	assert.Nil(t, err)
@@ -128,7 +127,7 @@ func TestNumeric(t *testing.T) {
 	fmt.Println(docs)
 
 	// Try "Explain"
-	explain, err := c.Explain(redisearch.NewQuery("hello world @bar:[40 90]"))
+	explain, err := c.Explain(NewQuery("hello world @bar:[40 90]"))
 	assert.Nil(t, err)
 	assert.NotNil(t, explain)
 	fmt.Println(explain)
@@ -138,9 +137,9 @@ func TestNoIndex(t *testing.T) {
 	c := createClient("testung")
 	c.Drop()
 
-	sc := redisearch.NewSchema(redisearch.DefaultOptions).
-		AddField(redisearch.NewTextFieldOptions("f1", redisearch.TextFieldOptions{Sortable: true, NoIndex: true, Weight: 1.0})).
-		AddField(redisearch.NewTextField("f2"))
+	sc := NewSchema(DefaultOptions).
+		AddField(NewTextFieldOptions("f1", TextFieldOptions{Sortable: true, NoIndex: true, Weight: 1.0})).
+		AddField(NewTextField("f2"))
 
 	err := c.CreateIndex(sc)
 	assert.Nil(t, err)
@@ -149,26 +148,26 @@ func TestNoIndex(t *testing.T) {
 	props["f1"] = "MarkZZ"
 	props["f2"] = "MarkZZ"
 
-	err = c.Index(redisearch.Document{Id: "doc1", Properties: props})
+	err = c.Index(Document{Id: "doc1", Properties: props})
 	assert.Nil(t, err)
 
 	props["f1"] = "MarkAA"
 	props["f2"] = "MarkAA"
-	err = c.Index(redisearch.Document{Id: "doc2", Properties: props})
+	err = c.Index(Document{Id: "doc2", Properties: props})
 	assert.Nil(t, err)
 
-	_, total, err := c.Search(redisearch.NewQuery("@f1:Mark*"))
+	_, total, err := c.Search(NewQuery("@f1:Mark*"))
 	assert.Nil(t, err)
 	assert.Equal(t, 0, total)
 
-	_, total, err = c.Search(redisearch.NewQuery("@f2:Mark*"))
+	_, total, err = c.Search(NewQuery("@f2:Mark*"))
 	assert.Equal(t, 2, total)
 
-	docs, total, err := c.Search(redisearch.NewQuery("@f2:Mark*").SetSortBy("f1", false))
+	docs, total, err := c.Search(NewQuery("@f2:Mark*").SetSortBy("f1", false))
 	assert.Equal(t, 2, total)
 	assert.Equal(t, "doc1", docs[0].Id)
 
-	docs, total, err = c.Search(redisearch.NewQuery("@f2:Mark*").SetSortBy("f1", true))
+	docs, total, err = c.Search(NewQuery("@f2:Mark*").SetSortBy("f1", true))
 	assert.Equal(t, 2, total)
 	assert.Equal(t, "doc2", docs[0].Id)
 }
@@ -176,19 +175,19 @@ func TestNoIndex(t *testing.T) {
 func TestHighlight(t *testing.T) {
 	c := createClient("testung")
 
-	sc := redisearch.NewSchema(redisearch.DefaultOptions).
-		AddField(redisearch.NewTextField("foo")).
-		AddField(redisearch.NewTextField("bar"))
+	sc := NewSchema(DefaultOptions).
+		AddField(NewTextField("foo")).
+		AddField(NewTextField("bar"))
 	c.Drop()
 	assert.Nil(t, c.CreateIndex(sc))
 
-	docs := make([]redisearch.Document, 100)
+	docs := make([]Document, 100)
 	for i := 0; i < 100; i++ {
-		docs[i] = redisearch.NewDocument(fmt.Sprintf("doc%d", i), 1).Set("foo", "hello world").Set("bar", "hello world foo bar baz")
+		docs[i] = NewDocument(fmt.Sprintf("doc%d", i), 1).Set("foo", "hello world").Set("bar", "hello world foo bar baz")
 	}
 	c.Index(docs...)
 
-	q := redisearch.NewQuery("hello").Highlight([]string{"foo"}, "[", "]")
+	q := NewQuery("hello").Highlight([]string{"foo"}, "[", "]")
 	docs, _, err := c.Search(q)
 	assert.Nil(t, err)
 
@@ -198,7 +197,7 @@ func TestHighlight(t *testing.T) {
 		assert.Equal(t, "hello world foo bar baz", d.Properties["bar"])
 	}
 
-	q = redisearch.NewQuery("hello world baz").Highlight([]string{"foo", "bar"}, "{", "}")
+	q = NewQuery("hello world baz").Highlight([]string{"foo", "bar"}, "{", "}")
 	docs, _, err = c.Search(q)
 	assert.Nil(t, err)
 
@@ -209,7 +208,7 @@ func TestHighlight(t *testing.T) {
 	}
 
 	// test RETURN contradicting HIGHLIGHT
-	q = redisearch.NewQuery("hello").Highlight([]string{"foo"}, "[", "]").SetReturnFields("bar")
+	q = NewQuery("hello").Highlight([]string{"foo"}, "[", "]").SetReturnFields("bar")
 	docs, _, err = c.Search(q)
 	assert.Nil(t, err)
 
@@ -225,20 +224,20 @@ func TestHighlight(t *testing.T) {
 func TestSammurize(t *testing.T) {
 	c := createClient("testung")
 
-	sc := redisearch.NewSchema(redisearch.DefaultOptions).
-		AddField(redisearch.NewTextField("foo")).
-		AddField(redisearch.NewTextField("bar"))
+	sc := NewSchema(DefaultOptions).
+		AddField(NewTextField("foo")).
+		AddField(NewTextField("bar"))
 	c.Drop()
 	assert.Nil(t, c.CreateIndex(sc))
 
-	docs := make([]redisearch.Document, 10)
+	docs := make([]Document, 10)
 	for i := 0; i < 10; i++ {
-		docs[i] = redisearch.NewDocument(fmt.Sprintf("doc%d", i), 1).
+		docs[i] = NewDocument(fmt.Sprintf("doc%d", i), 1).
 			Set("foo", "There are two sub-commands commands used for highlighting. One is HIGHLIGHT which surrounds matching text with an open and/or close tag; and the other is SUMMARIZE which splits a field into contextual fragments surrounding the found terms. It is possible to summarize a field, highlight a field, or perform both actions in the same query.").Set("bar", "hello world foo bar baz")
 	}
 	c.Index(docs...)
 
-	q := redisearch.NewQuery("commands fragments fields").Summarize("foo")
+	q := NewQuery("commands fragments fields").Summarize("foo")
 	docs, _, err := c.Search(q)
 	assert.Nil(t, err)
 
@@ -248,9 +247,9 @@ func TestSammurize(t *testing.T) {
 		assert.Equal(t, "hello world foo bar baz", d.Properties["bar"])
 	}
 
-	q = redisearch.NewQuery("commands fragments fields").
+	q = NewQuery("commands fragments fields").
 		Highlight([]string{"foo"}, "[", "]").
-		SummarizeOptions(redisearch.SummaryOptions{
+		SummarizeOptions(SummaryOptions{
 			Fields:       []string{"foo"},
 			Separator:    "\r\n",
 			FragmentLen:  10,
@@ -270,10 +269,10 @@ func TestTags(t *testing.T) {
 	c := createClient("myIndex")
 
 	// Create a schema
-	sc := redisearch.NewSchema(redisearch.DefaultOptions).
-		AddField(redisearch.NewTextField("title")).
-		AddField(redisearch.NewTagFieldOptions("tags", redisearch.TagFieldOptions{Separator: ';'})).
-		AddField(redisearch.NewTagField("tags2"))
+	sc := NewSchema(DefaultOptions).
+		AddField(NewTextField("title")).
+		AddField(NewTagFieldOptions("tags", TagFieldOptions{Separator: ';'})).
+		AddField(NewTagField("tags2"))
 
 	// Drop an existing index. If the index does not exist an error is returned
 	c.Drop()
@@ -284,19 +283,19 @@ func TestTags(t *testing.T) {
 	}
 
 	// Create a document with an id and given score
-	doc := redisearch.NewDocument("doc1", 1.0)
+	doc := NewDocument("doc1", 1.0)
 	doc.Set("title", "Hello world").
 		Set("tags", "foo bar;bar,baz;  hello world").
 		Set("tags2", "foo bar;bar,baz;  hello world")
 
 	// Index the document. The API accepts multiple documents at a time
-	if err := c.IndexOptions(redisearch.DefaultIndexingOptions, doc); err != nil {
+	if err := c.IndexOptions(DefaultIndexingOptions, doc); err != nil {
 		log.Fatal(err)
 	}
 
 	assertNumResults := func(q string, n int) {
 		// Searching with limit and sorting
-		_, total, err := c.Search(redisearch.NewQuery(q))
+		_, total, err := c.Search(NewQuery(q))
 		assert.Nil(t, err)
 
 		assert.Equal(t, n, total)
@@ -320,16 +319,16 @@ func TestSuggest(t *testing.T) {
 	a := createAutocompleter("testing")
 
 	// Add Terms to the Autocompleter
-	terms := make([]redisearch.Suggestion, 10)
+	terms := make([]Suggestion, 10)
 	for i := 0; i < 10; i++ {
-		terms[i] = redisearch.Suggestion{Term: fmt.Sprintf("foo %d", i),
+		terms[i] = Suggestion{Term: fmt.Sprintf("foo %d", i),
 			Score: 1.0, Payload: fmt.Sprintf("bar %d", i)}
 	}
 	err := a.AddTerms(terms...)
 	assert.Nil(t, err)
 
 	// Retrieve Terms From Autocompleter - Without Payloads / Scores
-	suggestions, err := a.SuggestOpts("f", redisearch.SuggestOptions{Num: 10})
+	suggestions, err := a.SuggestOpts("f", SuggestOptions{Num: 10})
 	assert.Nil(t, err)
 	assert.Equal(t, 10, len(suggestions))
 	for _, suggestion := range suggestions {
@@ -339,7 +338,7 @@ func TestSuggest(t *testing.T) {
 	}
 
 	// Retrieve Terms From Autocompleter - With Payloads & Scores
-	suggestions, err = a.SuggestOpts("f", redisearch.SuggestOptions{Num: 10, WithScores: true, WithPayloads: true})
+	suggestions, err = a.SuggestOpts("f", SuggestOptions{Num: 10, WithScores: true, WithPayloads: true})
 	assert.Nil(t, err)
 	assert.Equal(t, 10, len(suggestions))
 	for _, suggestion := range suggestions {
@@ -352,24 +351,24 @@ func TestSuggest(t *testing.T) {
 func TestDelete(t *testing.T) {
 	c := createClient("testung")
 
-	sc := redisearch.NewSchema(redisearch.DefaultOptions).
-		AddField(redisearch.NewTextField("foo"))
+	sc := NewSchema(DefaultOptions).
+		AddField(NewTextField("foo"))
 
 	err := c.Drop()
 	assert.Nil(t, err)
 	assert.Nil(t, c.CreateIndex(sc))
 
-	var info *redisearch.IndexInfo
+	var info *IndexInfo
 
 	// validate that the index is empty
 	info, err = c.Info()
 	assert.Nil(t, err)
 	assert.Equal(t, uint64(0), info.DocCount)
 
-	doc := redisearch.NewDocument("doc1", 1.0)
+	doc := NewDocument("doc1", 1.0)
 	doc.Set("foo", "Hello world")
 
-	err = c.IndexOptions(redisearch.DefaultIndexingOptions, doc)
+	err = c.IndexOptions(DefaultIndexingOptions, doc)
 	assert.Nil(t, err)
 
 	// now we should have 1 document (id = doc1)
@@ -394,10 +393,10 @@ func ExampleClient() {
 	c := createClient("myIndex")
 
 	// Create a schema
-	sc := redisearch.NewSchema(redisearch.DefaultOptions).
-		AddField(redisearch.NewTextField("body")).
-		AddField(redisearch.NewTextFieldOptions("title", redisearch.TextFieldOptions{Weight: 5.0, Sortable: true})).
-		AddField(redisearch.NewNumericField("date"))
+	sc := NewSchema(DefaultOptions).
+		AddField(NewTextField("body")).
+		AddField(NewTextFieldOptions("title", TextFieldOptions{Weight: 5.0, Sortable: true})).
+		AddField(NewNumericField("date"))
 
 	// Drop an existing index. If the index does not exist an error is returned
 	c.Drop()
@@ -408,18 +407,18 @@ func ExampleClient() {
 	}
 
 	// Create a document with an id and given score
-	doc := redisearch.NewDocument("doc1", 1.0)
+	doc := NewDocument("doc1", 1.0)
 	doc.Set("title", "Hello world").
 		Set("body", "foo bar").
 		Set("date", time.Now().Unix())
 
 	// Index the document. The API accepts multiple documents at a time
-	if err := c.IndexOptions(redisearch.DefaultIndexingOptions, doc); err != nil {
+	if err := c.IndexOptions(DefaultIndexingOptions, doc); err != nil {
 		log.Fatal(err)
 	}
 
 	// Searching with limit and sorting
-	docs, total, err := c.Search(redisearch.NewQuery("hello world").
+	docs, total, err := c.Search(NewQuery("hello world").
 		Limit(0, 2).
 		SetReturnFields("title"))
 
